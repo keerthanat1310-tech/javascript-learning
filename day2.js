18f8550ce7fb3ed241a61f67877b7582267b2d5b
keerthanat1310-tech <keerthanat1310@gmail.com>
2026-03-15 14:02:32 +0000

    save day 2 java script

---
+++ day2.js
@@ -0,0 +1,9 @@
+const name="KEERTHANA T";
+let id="130";
+let place="kolar";
+email ="keerthanat1310@gmail.com";
+
+id="137";
+place="bengaluru";
+email="keerthuuuu@gmail.com";
+console.table([name,id,place,email]);
