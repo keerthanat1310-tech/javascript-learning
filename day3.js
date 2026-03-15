dd4dd46e406271c80fc490524e4c3fc0a2c95516
keerthanat1310-tech <keerthanat1310@gmail.com>
2026-03-15 16:03:28 +0000

    javascript learnig in process

---
+++ day3.js
@@ -0,0 +1,8 @@
+console.log(Math.abs(-89));
+console.log(Math.round(5.8));
+console.log(Math.min(3,89,67,1));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+const min=1;
+const max=6;
+console.log((Math.floor(Math.random()*(max-min+1)+min)));
